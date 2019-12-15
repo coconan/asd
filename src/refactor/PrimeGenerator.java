e85f83bf38ce1e27525849520bc6eded5d31d628
yangtaimeng <yangtaimeng@yidaoit.cn>
2019-12-15 20:46:50 +0800

    Primes version 4

--- src/refactor/PrimeGenerator.java
+++ src/refactor/PrimeGenerator.java
@@ -13,7 +13,7 @@ package refactor;
  */
 public class PrimeGenerator {
 
-    private static boolean[] f;
+    private static boolean[] isCrossed;
     private static int[] result;
 
     /**
@@ -36,8 +36,8 @@ public class PrimeGenerator {
 
         // how many primes are there?
         int count = 0;
-        for (i = 0; i < f.length; i++) {
-            if (f[i]) {
+        for (i = 2; i < isCrossed.length; i++) {
+            if (notCrossed(i)) {
                 count++;   // bump count
             }
         }
@@ -45,34 +45,47 @@ public class PrimeGenerator {
         result = new int[count];
 
         // move the primes into the result
-        for (i = 0, j = 0; i < f.length; i++) {
-            if (f[i]) {    // if prime
+        for (i = 2, j = 0; i < isCrossed.length; i++) {
+            if (notCrossed(i)) {    // if prime
                 result[j++] = i;
             }
         }
     }
 
     private static void crossOutMultiples() {
-        int i;
-        int j;
-
-        for (i = 2; i < Math.sqrt(f.length) + 1; i++) {
-            if (f[i]) {  // if i is uncrossed, cross out its multiples.
-                for (j = 2 * i; j < f.length; j+=i) {
-                    f[j] = false;   // multiple is not prime
-                }
+        int maxPrimeFactor = calcMaxPrimeFactor();
+        for (int i = 2; i <= maxPrimeFactor; i++) {
+            if (notCrossed(i)) {  // if i is uncrossed, cross out its multiples.
+                crossOutMultiplesOf(i);
             }
         }
     }
 
+    private static int calcMaxPrimeFactor() {
+        // We cross out all multiples of p; where p is prime.
+        // Thus, all crossed out multiples have p and q for
+        // factors. If p > sqrt of the size of the array, then
+        // q will never be greater than 1. Thus p is the
+        // largest prime factor in the array, and is also
+        // the iteration limit.
+        double maxPrimeFactor = Math.sqrt(isCrossed.length) + 1;
+        return (int) maxPrimeFactor;
+    }
+
+    private static void crossOutMultiplesOf(int i) {
+        for (int multiple = 2*i; multiple < isCrossed.length; multiple += i) {
+            isCrossed[multiple] = true;
+        }
+    }
+
+    private static boolean notCrossed(int i) {
+        return !isCrossed[i];
+    }
+
     private static void initializeArrayOfIntegers(int maxValue) {
-        // declarations
-        f = new boolean[maxValue + 1];
-        f[0] = f[1] = false;
-        int i;
-        // initialize array to true.
-        for (i = 2; i < f.length; i++) {
-            f[i] = true;
+        isCrossed = new boolean[maxValue + 1];
+        for (int i = 2; i < isCrossed.length; i++) {
+            isCrossed[i] = false;
         }
     }
 }
