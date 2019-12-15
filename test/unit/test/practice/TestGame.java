f337fffabafe29df6a2d8a347b15d57b1ddbfa7e
yangtaimeng <yangtaimeng@yidaoit.cn>
2019-12-15 22:36:32 +0800

    add multiple throws

--- test/unit/test/practice/TestGame.java
+++ test/unit/test/practice/TestGame.java
@@ -14,10 +14,29 @@ public class TestGame {
     }
 
     @Test
-    public void testAddOneThrow()
-    {
+    public void testAddOneThrow() {
         Game f = new Game();
         f.add(5);
         assertEquals(5, f.getScore());
+    }
+
+    @Test
+    public void testTwoThrowsNoMark() {
+        Game g = new Game();
+        g.add(5);
+        g.add(4);
+        assertEquals(9, g.getScore());
+    }
+
+    @Test
+    public void testFourThrowsNoMark() {
+        Game g = new Game();
+        g.add(5);
+        g.add(4);
+        g.add(7);
+        g.add(2);
+        assertEquals(18, g.getScore());
+        assertEquals(9, g.getScoreForFrame(1));
+        assertEquals(18, g.getScoreForFrame(2));
     }
 }
