a5c2a664d29dc837b1482bba5d2f12fd40a57c11
yangtaimeng <yangtaimeng@yidaoit.cn>
2019-12-16 21:29:52 +0800

    refactor frame logic - move strike advance logic to its own method

--- src/practice/Game.java
+++ src/practice/Game.java
@@ -17,15 +17,21 @@ public class Game {
 
     private void adjustCurrentFrame(int pins) {
         if (firstThrowInFrame) {
-            if (pins == 10) {  // strike
-                advanceFrame();
-            } else {
+            if (!adjustFrameForStrike(pins)) {
                 firstThrowInFrame = false;
             }
         } else {
             firstThrowInFrame = true;
             advanceFrame();
         }
+    }
+
+    private boolean adjustFrameForStrike(int pins) {
+        if (pins == 10) {
+            advanceFrame();
+            return true;
+        }
+        return false;
     }
 
     private void advanceFrame() {
