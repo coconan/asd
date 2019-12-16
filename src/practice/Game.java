17cefbeedd90c9de69ee34c6d65c77ad545cf567
yangtaimeng <yangtaimeng@yidaoit.cn>
2019-12-16 21:21:49 +0800

    refactor - separate score logic from Game

--- src/practice/Game.java
+++ src/practice/Game.java
@@ -2,18 +2,16 @@ package practice;
 
 public class Game {
 
-    private int[] itsThrows = new int[21];
-    private int itsCurrentThrow = 0;
     private int itsCurrentFrame = 1;
     private boolean firstThrowInFrame = true;
-    private int ball;
+    private Scorer itsScorer = new Scorer();
 
     public int getScore() {
         return getScoreForFrame(getCurrentFrame() - 1);
     }
 
     public void add(int pins) {
-        itsThrows[itsCurrentThrow++] = pins;
+        itsScorer.addThrow(pins);
         adjustCurrentFrame(pins);
     }
 
@@ -32,42 +30,9 @@ public class Game {
     }
 
     public int getScoreForFrame(int theFrame) {
-        ball = 0;
-        int score = 0;
-        for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-            if (strike()) {
-                score += 10 + nextTwoBallsForStrike();
-                ball++;
-            } else if (spare()) {
-                score += 10 + nextBallForSpare();
-                ball += 2;
-            } else {
-                score += twoBallsInFrame();
-                ball += 2;
-            }
-        }
-        return score;
+        return itsScorer.scoreForFrame(theFrame);
     }
 
-    private boolean strike() {
-        return itsThrows[ball] == 10;
-    }
-
-    private int nextTwoBallsForStrike() {
-        return itsThrows[ball + 1] + itsThrows[ball + 2];
-    }
-
-    private int twoBallsInFrame() {
-        return itsThrows[ball] + itsThrows[ball + 1];
-    }
-
-    private boolean spare() {
-        return (itsThrows[ball] + itsThrows[ball + 1]) == 10;
-    }
-
-    private int nextBallForSpare() {
-        return itsThrows[ball+2];
-    }
 
     public int getCurrentFrame() {
         return itsCurrentFrame;
