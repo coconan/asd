17aa49c9819c2a1852b80ce47810f781364d9d10
yangtaimeng <yangtaimeng@yidaoit.cn>
2019-12-16 21:26:28 +0800

    refactor frame logic - move advance logic to its own method

--- src/practice/Game.java
+++ src/practice/Game.java
@@ -18,15 +18,18 @@ public class Game {
     private void adjustCurrentFrame(int pins) {
         if (firstThrowInFrame) {
             if (pins == 10) {  // strike
-                itsCurrentFrame++;
+                advanceFrame();
             } else {
                 firstThrowInFrame = false;
             }
         } else {
             firstThrowInFrame = true;
-            itsCurrentFrame++;
+            advanceFrame();
         }
-        itsCurrentFrame = Math.min(11, itsCurrentFrame);
+    }
+
+    private void advanceFrame() {
+        itsCurrentFrame = Math.min(11, itsCurrentFrame + 1);
     }
 
     public int getScoreForFrame(int theFrame) {
